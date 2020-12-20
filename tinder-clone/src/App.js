e338963f986eba36620946bbd0bab08eebea5fc8
frank <franktranvantu@gmail.com>
2020-12-21 00:02:46 +0700

    update

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
